2d0bad4dacb60818a226ca330f81f6a72a257b05
huynq2zigexn <huynq2@zigexn.vn>
2024-06-10 15:00:48 +0700

    add exports

---
+++ app/Filament/Export/RecruitExport.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Filament\Export;
+
+use App\Models\Recruit;
+use Filament\Actions\Exports\ExportColumn;
+use Filament\Actions\Exports\Exporter;
+use Filament\Actions\Exports\Models\Export;
+
+class RecruitExport extends Exporter
+{
+    protected static ?string $model = Recruit::class;
+
+    public static function getColumns(): array
+    {
+        return [
+            ExportColumn::make('name'),
+            ExportColumn::make('start_date'),
+            ExportColumn::make('end_date'),
+        ];
+    }
+
+    public static function getCompletedNotificationBody(Export $export): string
+    {
+        $body = 'Your Recruit export has completed and ' . number_format($export->successful_rows) . ' ' . str('row')->plural($export->successful_rows) . ' exported.';
+
+        if ($failedRowsCount = $export->getFailedRowsCount()) {
+            $body .= ' ' . number_format($failedRowsCount) . ' ' . str('row')->plural($failedRowsCount) . ' failed to export.';
+        }
+
+        return $body;
+    }
+}
--- app/Filament/Resources/RecruitResource/Pages/ListRecruits.php
+++ app/Filament/Resources/RecruitResource/Pages/ListRecruits.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\RecruitResource\Pages;
 
+use App\Filament\Export\RecruitExport;
 use App\Filament\Resources\RecruitResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
@@ -13,6 +14,8 @@ class ListRecruits extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            Actions\ExportAction::make()
+                ->exporter(RecruitExport::class),
             Actions\CreateAction::make(),
         ];
     }
